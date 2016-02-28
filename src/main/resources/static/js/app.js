fc36fc7b2e2521972ead237a0f8d650fb1593e24
sakuya3834 <sakuya3834@gmail.com>
2016-02-28 00:18:54 -0800

    third pass at table, almost there

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,4 +1,4 @@
-var app = angular.module('app', ['ngAnimate', 'ngAria', 'ngMaterial'])
+var app = angular.module('app', ['ngAnimate', 'ngAria', 'ngMaterial', 'ui.grid'])
 
 // Define directives here
 app.directive('tabset', function () {
@@ -8,7 +8,7 @@ app.directive('tabset', function () {
     templateUrl: 'tabset.html',
     bindToController: true,
     controllerAs: 'tabset',
-		controller: function () {
+    controller: function () {
       var self = this
       self.tabs = []
       self.addTab = function addTab (tab) {
@@ -55,14 +55,32 @@ app.directive('content', function () {
 })
 
 app.controller('mytable', function ($scope, $http) {
-  $scope.loadData = function () {
-		$scope.launches = []
-    $http.get('./upcoming_launches.json')
-      .success(function (data) {
-				$scope.launches = data.upcomingLaunches
-      })
-  }
-  $scope.loadData()
+
+	$scope.gridOptions = {};
+  $scope.gridOptions = {
+		columnDefs: [
+    { field: 'date', displayName: 'Date', minWidth: 70, maxWidth: 150},
+    { field: 'launchVehicle', displayName: 'Vehicle', minWidth: 70, maxWidth: 150},
+    { field: 'launchLocation', displayName: 'Location', width: '*'},
+    { field: 'payload', displayName: 'Payload', minWidth: 70, maxWidth: 150},
+    { field: 'description', displayName: 'More Info', cellTooltip: '{{row.entity.description}}',
+      cellTemplate: '<div class="ui-grid-cell-contents"><md-content><md-button><md-icon md-svg-src="./img/ic_info_outline_black_24px.svg" style="width: 24px; height: 24px; margin-right: 20; margin:bottom: 10;"></md-icon><md-tooltip md-direction="left" style="width: 300px; height: 300px;">{{row.entity.description}}</md-tooltip></md-button><md-content></div>',
+      minWidth: 50, maxWidth: 100}
+  ],
+	rowHeight: '6%'
+}
+
+
+$scope.loadData = function () {
+	$scope.launches = []
+	$http.get('./upcoming_launches.json')
+		.success(function (data) {
+			$scope.gridOptions.data = data.upcomingLaunches
+		})
+}
+
+$scope.loadData()
+
 })
 
 app.directive('mytable', function () {
